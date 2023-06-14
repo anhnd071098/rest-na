0f19c44c9106fce99d9e0490abb6e35b8a75db71
anhnd071098 <anhnd071098@gmail.com>
2023-06-14 16:18:17 +0700

    Initial commit

---
+++ src/main/java/vn/pdsoft/restna/RestNaApplication.java
@@ -0,0 +1,13 @@
+package vn.pdsoft.restna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestNaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestNaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/pdsoft/restna/RestNaApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.pdsoft.restna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestNaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
